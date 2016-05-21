422aa46dcba287a0f07e18bd898145a965bcb1d7
mehanig <mehanig@gmail.com>
2016-05-22 01:52:07 +0300

    encoded data in headers

--- updater.js
+++ updater.js
@@ -541,137 +541,137 @@ if (typeof JSON !== 'object') {
 **/
 var Base64 = {
 
-// private property
-_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+    // private property
+    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
 
-// public method for encoding
-encode : function (input) {
-    var output = "";
-    var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-    var i = 0;
+    // public method for encoding
+    encode : function (input) {
+        var output = "";
+        var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+        var i = 0;
 
-    input = Base64._utf8_encode(input);
+        input = Base64._utf8_encode(input);
 
-    while (i < input.length) {
+        while (i < input.length) {
 
-        chr1 = input.charCodeAt(i++);
-        chr2 = input.charCodeAt(i++);
-        chr3 = input.charCodeAt(i++);
+            chr1 = input.charCodeAt(i++);
+            chr2 = input.charCodeAt(i++);
+            chr3 = input.charCodeAt(i++);
 
-        enc1 = chr1 >> 2;
-        enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-        enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-        enc4 = chr3 & 63;
+            enc1 = chr1 >> 2;
+            enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+            enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+            enc4 = chr3 & 63;
 
-        if (isNaN(chr2)) {
-            enc3 = enc4 = 64;
-        } else if (isNaN(chr3)) {
-            enc4 = 64;
+            if (isNaN(chr2)) {
+                enc3 = enc4 = 64;
+            } else if (isNaN(chr3)) {
+                enc4 = 64;
+            }
+
+            output = output +
+            this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+            this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+
         }
 
-        output = output +
-        this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
-        this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+        return output;
+    },
 
-    }
+    // public method for decoding
+    decode : function (input) {
+        var output = "";
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
 
-    return output;
-},
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
 
-// public method for decoding
-decode : function (input) {
-    var output = "";
-    var chr1, chr2, chr3;
-    var enc1, enc2, enc3, enc4;
-    var i = 0;
+        while (i < input.length) {
 
-    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+            enc1 = this._keyStr.indexOf(input.charAt(i++));
+            enc2 = this._keyStr.indexOf(input.charAt(i++));
+            enc3 = this._keyStr.indexOf(input.charAt(i++));
+            enc4 = this._keyStr.indexOf(input.charAt(i++));
 
-    while (i < input.length) {
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
 
-        enc1 = this._keyStr.indexOf(input.charAt(i++));
-        enc2 = this._keyStr.indexOf(input.charAt(i++));
-        enc3 = this._keyStr.indexOf(input.charAt(i++));
-        enc4 = this._keyStr.indexOf(input.charAt(i++));
+            output = output + String.fromCharCode(chr1);
 
-        chr1 = (enc1 << 2) | (enc2 >> 4);
-        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-        chr3 = ((enc3 & 3) << 6) | enc4;
+            if (enc3 != 64) {
+                output = output + String.fromCharCode(chr2);
+            }
+            if (enc4 != 64) {
+                output = output + String.fromCharCode(chr3);
+            }
 
-        output = output + String.fromCharCode(chr1);
-
-        if (enc3 != 64) {
-            output = output + String.fromCharCode(chr2);
-        }
-        if (enc4 != 64) {
-            output = output + String.fromCharCode(chr3);
         }
 
-    }
+        output = Base64._utf8_decode(output);
 
-    output = Base64._utf8_decode(output);
+        return output;
 
-    return output;
+    },
 
-},
+    // private method for UTF-8 encoding
+    _utf8_encode : function (string) {
+        string = string.replace(/\r\n/g,"\n");
+        var utftext = "";
 
-// private method for UTF-8 encoding
-_utf8_encode : function (string) {
-    string = string.replace(/\r\n/g,"\n");
-    var utftext = "";
+        for (var n = 0; n < string.length; n++) {
 
-    for (var n = 0; n < string.length; n++) {
+            var c = string.charCodeAt(n);
 
-        var c = string.charCodeAt(n);
+            if (c < 128) {
+                utftext += String.fromCharCode(c);
+            }
+            else if((c > 127) && (c < 2048)) {
+                utftext += String.fromCharCode((c >> 6) | 192);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
+            else {
+                utftext += String.fromCharCode((c >> 12) | 224);
+                utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                utftext += String.fromCharCode((c & 63) | 128);
+            }
 
-        if (c < 128) {
-            utftext += String.fromCharCode(c);
         }
-        else if((c > 127) && (c < 2048)) {
-            utftext += String.fromCharCode((c >> 6) | 192);
-            utftext += String.fromCharCode((c & 63) | 128);
-        }
-        else {
-            utftext += String.fromCharCode((c >> 12) | 224);
-            utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-            utftext += String.fromCharCode((c & 63) | 128);
-        }
 
-    }
-
-    return utftext;
-},
+        return utftext;
+    },
 
 // private method for UTF-8 decoding
-_utf8_decode : function (utftext) {
-    var string = "";
-    var i = 0;
-    var c = c1 = c2 = 0;
+    _utf8_decode : function (utftext) {
+        var string = "";
+        var i = 0;
+        var c = c1 = c2 = 0;
 
-    while ( i < utftext.length ) {
+        while ( i < utftext.length ) {
 
-        c = utftext.charCodeAt(i);
+            c = utftext.charCodeAt(i);
 
-        if (c < 128) {
-            string += String.fromCharCode(c);
-            i++;
+            if (c < 128) {
+                string += String.fromCharCode(c);
+                i++;
+            }
+            else if((c > 191) && (c < 224)) {
+                c2 = utftext.charCodeAt(i+1);
+                string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                i += 2;
+            }
+            else {
+                c2 = utftext.charCodeAt(i+1);
+                c3 = utftext.charCodeAt(i+2);
+                string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                i += 3;
+            }
+
         }
-        else if((c > 191) && (c < 224)) {
-            c2 = utftext.charCodeAt(i+1);
-            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-            i += 2;
-        }
-        else {
-            c2 = utftext.charCodeAt(i+1);
-            c3 = utftext.charCodeAt(i+2);
-            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-            i += 3;
-        }
 
+        return string;
     }
-
-    return string;
-}
 
 }
 
@@ -735,9 +735,16 @@ var updater = {};
         var response = null;
         var wincurl = updater.MHNG_WORKING_DIR + "\\aeupdater" + "\\" + "curl.vbs"; //the path to the .vbs file
         var curlCmd = '';
+        var user_stats;
+        var stats_data = {
+            user_id: updater.MHNG_USER_UID
+            os: 'undefined'
+        };
         try {
             if (updater.os() == "Win") {
+                stats_data.os = "Win";
                 updater.createAeUpdatesFolderIfNone();
+                user_stats = Base64.encode("aupdatesdata:" + JSON.stringify(stats_data));
                 var vbsFile = new File(updater.MHNG_WORKING_DIR + "\\" + "aeupdater" + "\\" + "curl.vbs");
                 vbsFile.open("w");
                 vbsFile.encoding = "UTF-8";
@@ -746,10 +753,12 @@ var updater = {};
                 vbsFile.close();
                 curlCmd = 'cscript "' + wincurl + '" /Method:' + method + ' /URL:' + endpoint + ' /Query:' + query + ' //nologo';
             } else {
+                stats_data.os = "Osx"
                 if (method === "POST") {
-                    curlCmd = 'curl -m 4 -s -d "' + query + '" ' + endpoint;
+                    user_stats = Base64.encode(JSON.stringify(stats_data));
+                    curlCmd = 'curl -m 4 -s -H ' + user_stats +' -d "' + query + '" ' + endpoint;
                 } else if (method === "GET") {
-                    curlCmd = 'curl -m 4 -s -G -d "' + query + '" ' + endpoint;
+                    curlCmd = 'curl -m 4 -s -H ' + user_stats +' -G -d "' + query + '" ' + endpoint;
                 }
             }
             response = system.callSystem(curlCmd);
